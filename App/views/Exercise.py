39a36c9aa46374d4e66d110f4613f6e527d89e70
BibiSurienaAlly <surienaally4h@gmail.com>
2023-04-19 22:32:15 +0000

    added exercise and user exercise views

---
+++ App/views/Exercise.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
+from flask_jwt_extended import jwt_required, current_user as jwt_current_user
+from flask_login import current_user, login_required
+
+from.index import index_views
+
+from App.controllers import (
+    create_Exercise,
+    get_exercise,
+    get_exercise_by_name,
+    get_all_exercises
+)
+
+Exercise_views = Blueprint('Exercise_views', name, template_folder='../templates')
+
+@Exercise_views.route('/exercises', methods=['GET'])
+def get_Exercise_page():
+    exercises = get_all_exercises()
+    return render_template('profile.html', exercise=exercises)
+
+
+@Exercise_views.route('/exercises/<int: id>', methods=['GET'])
+def get_Exercise_Action(id):
+    exercises = get_exercise(id)
+    return render_template('profile.html', exercise=exercises)
+
+
+@Exercise_views.route('/exercises/<string: name>', methods=['GET'])
+def get_Exercise_Action(name):
+    exercises = get_exercise_by_name(name)
+    return render_template('profile.html', exercise=exercises)
---
+++ App/views/UserExercise.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
+from flask_jwt_extended import jwt_required, current_user as jwt_current_user
+from flask_login import current_user, login_required
+
+from.index import index_views
+
+from App.controllers import (
+    delete_userexercise,
+    update_userexercise,
+    get_all_userexercise,
+    get_userexercise,
+    create_UserExercise
+)
+
+userexercise_views = Blueprint('userexercise_views', __name__, template_folder='../templates')
+
+
+@userexercise_views.route('/userexercises', methods=['GET'])
+def get_userexercise_page():
+    userexercises = get_all_userexercise()
+    return render_template('profile.html', userExercise=userexercises)
+
+
+@userexercise_views.route('/userexercises/<int: id>', methods=['GET'])
+def get_userexercise_action(id):
+    userexercises = get_userexercise(id)
+    return render_template('profile.html', userExercise=userexercises)
+
+
+@userexercise_views.route('/userexercises/delete/<int: id>/', methods=['DELETE'])
+def deleteuserexercise_action(id):
+    userexercises = delete_userexercise(id)
+    return render_template('profile.html', userExercise=userexercises)
+
+@userexercise_views.route('/userexercises/update/<int: id>/', methods=['PUT'])
+def update_userexercise_action(id):
+    userexercise = update_userexercise(id)
+    return render_template('profile.html', userExercise=userexercises)
