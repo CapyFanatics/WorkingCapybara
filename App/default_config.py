de13deba49812560de6757ad89320cc33e2a30f1
Jardel - JD <mitchelljardel@gmail.com>
2023-04-18 21:42:29 -0400

    Update default_config.py

--- App/default_config.py
+++ App/default_config.py
@@ -1,4 +1,6 @@
-SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
+SQLALCHEMY_DATABASE_URI = "postgres://mydb_lbln_user:nxotEH0nJ8zoHBpG5QhtHr62rlJDQ14t@dpg-cgvg0kpjvhtmig3mckug-a.oregon-postgres.render.com/mydb_lbln.ql"
 SECRET_KEY = "secret key"
 JWT_ACCESS_TOKEN_EXPIRES = 7
 ENV = "DEVELOPMENT"
+
+#postgres://mydb_lbln_user:nxotEH0nJ8zoHBpG5QhtHr62rlJDQ14t@dpg-cgvg0kpjvhtmig3mckug-a.oregon-postgres.render.com/mydb_lbln.ql
