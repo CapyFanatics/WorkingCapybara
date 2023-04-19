d9123e0d3e0ae2dc03a355f7ec93560149d80114
Jardel - JD <mitchelljardel@gmail.com>
2023-04-19 13:38:10 -0400

    default

--- App/models/user.py
+++ App/models/user.py
@@ -6,21 +6,10 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username =  db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    exercise = db.relationship('UserExercise', backref='user')
 
-    def init(self, username, password, email):
+    def __init__(self, username, password):
         self.username = username
-        self.email = email
         self.set_password(password)
-
-    def change_email(self, new_email):
-        self.email = new_email
-        self.save()
-
-    def change_password(self, new_password):
-        self.password = new_password
-        self.save()
 
     def get_json(self):
         return{
@@ -31,7 +20,7 @@ class User(db.Model, UserMixin):
     def set_password(self, password):
         """Create hashed password."""
         self.password = generate_password_hash(password, method='sha256')
-
+    
     def check_password(self, password):
         """Check hashed password."""
         return check_password_hash(self.password, password)
