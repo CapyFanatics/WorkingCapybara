5f088876189a25eb6f6fb5f89a12de3c5f070c4d
Jardel - JD <mitchelljardel@gmail.com>
2023-04-19 13:05:25 -0400

    test

--- App/models/user.py
+++ App/models/user.py
@@ -2,74 +2,6 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin
 from App.database import db
 
-class UserExercise(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    exercise_id = db.Column(db.Integer, db.ForeignKey('exercise.id'))
-    exercise = db.relationship('Exercise')
-    date = db.Column(db.DateTime, nullable=False)
-    reps = db.Column(db.Integer, nullable=False)
-    sets = db.Column(db.Integer, nullable=False)
-    weight = db.Column(db.Float)
-
-    def repr(self):
-      return f'<UserExercise {self.id} : {self.name} User {self.user.username}>'
-
-    def add_exercise(self, exercise_id, name, reps, sets):
-        exercise = Exercise.query.get(exercise_id)
-
-        if exercise:
-            try:
-                user_exercise = UserExercise(self.id, exercise_id, name, reps, sets)
-                db.session.add(user_exercise)
-                db.session.commit()
-                return user_exercise
-            except Exception:
-                db.session.rollback()
-                return None
-
-        return None
-
-    def delete_exercise(self, exercise_id, name, reps, sets):
-        exercise = Exercise.query.get(exercise_id)
-
-        if exercise.user == self:
-            db.session.delete(exercise)
-            db.session.commit()
-            return True
-        return None
-
-    def edit_exercise(self, exercise_id, name, reps, sets):
-        exercise = Exercise.query.get(exercise_id)
-
-        if exercise.user == self:
-            exercise.name = name
-            exercise.reps = reps
-            exercise.sets = sets
-
-            db.session.add(exercise)
-            db.session.commit()
-            return True
-        return None
-
-
-
-
-    def get_json(self):
-        return{
-            'id' : self.id,
-            'name' : self.name,
-            'user' : self.user.username,
-            'date' : self.date,
-            'reps' : self.reps,
-            'sets' : self.sets,
-            'weight' : self.weight
-        }
-
-
-
-
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username =  db.Column(db.String, nullable=False, unique=True)
@@ -103,16 +35,3 @@ class User(db.Model, UserMixin):
     def check_password(self, password):
         """Check hashed password."""
         return check_password_hash(self.password, password)
-    
-    
-class Exercise(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), nullable=False)
-
-
-
-    def repr(self):
-        return{
-            'id' : self.id,
-            'name' : self.name
-        }
