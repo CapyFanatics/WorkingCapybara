1fad8ad576c22ac17dc4ac7f67bd497a4a018c46
Jardel - JD <mitchelljardel@gmail.com>
2023-04-21 09:00:20 -0400

    Update default_config.py

--- App/default_config.py
+++ App/default_config.py
@@ -3,4 +3,4 @@ SECRET_KEY = "secret key"
 JWT_ACCESS_TOKEN_EXPIRES = 7
 ENV = "DEVELOPMENT"
 
-#postgres://mydb_lbln_user:nxotEH0nJ8zoHBpG5QhtHr62rlJDQ14t@dpg-cgvg0kpjvhtmig3mckug-a.oregon-postgres.render.com/mydb_lbln.ql
+#postgres://mydb_lbln_user:nxotEH0nJ8zoHBpG5QhtHr62rlJDQ14t@dpg-cgvg0kpjvhtmig3mckug-a.oregon-postgres.render.com/mydb_lbln
