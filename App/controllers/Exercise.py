1708d0a15eba9dc2cf7f48e6c34784e2c5a2d8e6
Jardel - JD <95093809+Jdmitchel@users.noreply.github.com>
2023-04-22 11:57:09 -0400

    Bibi (#42)
    * pull (#30)
    
    * update user and userExercise models
    
    * Index header (#22)
    
    * Index header
    
    * update user and userExercise models
    
    * Vance's commit
    
    ---------
    
    Co-authored-by: BibiSurienaAlly <surienaally4h@gmail.com>
    Co-authored-by: BibiSurienaAlly <97921869+BibiSurienaAlly@users.noreply.github.com>
    
    * init commit (#27)
    
    * Index header
    
    * Vance's commit
    
    * __init__ file imports the controllers and models
    
    * compare (#29)
    
    * Index header
    
    * Vance's commit
    
    * __init__ file imports the controllers and models
    
    ---------
    
    Co-authored-by: BibiSurienaAlly <surienaally4h@gmail.com>
    Co-authored-by: BibiSurienaAlly <97921869+BibiSurienaAlly@users.noreply.github.com>
    
    * Update equipment.html
    
    * many changes again
    
    ummm idek at this point
    
    * Bibi's commit
    
    Summarizes the changes over the past 2 days.
    
    Pages working with view
    db initialized
    
    Co-Authored-By: BibiSurienaAlly <97921869+BibiSurienaAlly@users.noreply.github.com>
    Co-Authored-By: Vance421 <97921396+Vance421@users.noreply.github.com>
    
    * Controller, Template and views modification
    
    * Update test_app.py
    
    * Update dev.yml
    
    ---------
    
    Co-authored-by: BibiSurienaAlly <surienaally4h@gmail.com>
    Co-authored-by: BibiSurienaAlly <97921869+BibiSurienaAlly@users.noreply.github.com>
    Co-authored-by: Vance421 <97921396+Vance421@users.noreply.github.com>

--- App/controllers/Exercise.py
+++ App/controllers/Exercise.py
@@ -1,6 +1,18 @@
 from App.models import Exercise
 from App.database import db
 
+import requests
+
+
+API_URL = 'https://wger.de/api/v2/exercise/?language=2'
+API_KEY = 'db41e887abdeee70f768105f746b93afa2a1e856'
+
+def get_api_data(API_URL, API_KEY):
+    response = requests.get(API_URL, headers=({'X-Api-Key': 'API_KEY'}) )
+    data = response.json()
+    #data = data['results'][1]['name']
+    return data
+
 
 def create_Exercise(name, exercise_type, muscle_group, difficulty):
     NewExercise = Exercise(name=name, exercise_type=exercise_type, muscle_group=muscle_group, difficulty=difficulty)
@@ -24,6 +36,7 @@ def get_exercise_by_difficulty(difficulty):
     return Exercise.query.filter_by(difficulty = difficulty)
 
 def get_exercise_by_name(name):
+
 
     return Exercise.query.filter_by(name=name).first()
     
--- App/views/auth.py
+++ App/views/auth.py
@@ -1,6 +1,14 @@
 from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
 from flask_jwt_extended import jwt_required, current_user as jwt_current_user
 from flask_login import login_required, login_user, current_user, logout_user
+import requests
+
+from App.models.user import User
+from App.models.Exercise import Exercise
+from App.models.userExercise import UserExercise
+from App.database import db
+
+from flask_login import LoginManager
 
 from App.models.user import User
 from App.models.Exercise import Exercise
@@ -15,10 +23,16 @@ from App.controllers import (
     create_user,
     jwt_authenticate,
     login,
-    get_exercise_by_type
+
+    get_exercise_by_type,
+    get_api_data
+
 )
 
 auth_views = Blueprint('auth_views', __name__, template_folder='../templates')
+
+API_URL = 'https://wger.de/api/v2/exercise/?language=2'
+API_KEY = 'db41e887abdeee70f768105f746b93afa2a1e856'
 
 '''
 Page/Action Routes
@@ -101,7 +115,10 @@ def logout_action():
 
 @auth_views.route('/equipment', methods=['GET'])
 def equipment_action():
-    return render_template("equipment.html")
+
+    data = get_api_data(API_URL, API_KEY)
+    return render_template("equipment.html", data=data['results'])
+
 
 '''
 API Routes
